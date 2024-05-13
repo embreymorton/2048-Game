29b6f51a870269f34984c333876f4c26ad586cca
embreymorton <111366508+embreymorton@users.noreply.github.com>
2024-05-13 15:03:23 -0400

    Implement Tile Dictionary

--- src/tile.py
+++ src/tile.py
@@ -19,6 +19,22 @@ class Tile:
         self.merged = merged
 
 
+# Dictionary of Tile Objects
+tiles = {
+    0: Tile(0, COLOR_EMPTY, WHITE),
+    2: Tile(2, COLOR_2, BLACK),
+    4: Tile(4, COLOR_4, BLACK),
+    8: Tile(8, COLOR_8, WHITE),
+    16: Tile(16, COLOR_16, WHITE),
+    32: Tile(32, COLOR_32, WHITE),
+    64: Tile(64, COLOR_64, WHITE),
+    128: Tile(128, COLOR_128, WHITE),
+    256: Tile(256, COLOR_256, WHITE),
+    512: Tile(512, COLOR_512, WHITE),
+    1024: Tile(1024, COLOR_1024, WHITE),
+    2048: Tile(2048, COLOR_2048, WHITE)
+}
+
 def get_tile(value):
     """
     Create and return a Tile object based on the given value.
@@ -29,30 +45,4 @@ def get_tile(value):
     Returns:
         Tile: A Tile object with the specified value and appropriate colors.
     """
-    match value:
-        case 0:
-            return Tile(0, COLOR_EMPTY, WHITE)
-        case 2:
-            return Tile(2, COLOR_2, BLACK)
-        case 4:
-            return Tile(4, COLOR_4, BLACK)
-        case 8:
-            return Tile(8, COLOR_8, WHITE)
-        case 16:
-            return Tile(16, COLOR_16, WHITE)
-        case 32:
-            return Tile(32, COLOR_32, WHITE)
-        case 64:
-            return Tile(64, COLOR_64, WHITE)
-        case 128:
-            return Tile(128, COLOR_128, WHITE)
-        case 256:
-            return Tile(256, COLOR_256, WHITE)
-        case 512:
-            return Tile(512, COLOR_512, WHITE)
-        case 1024:
-            return Tile(1024, COLOR_1024, WHITE)
-        case 2048:
-            return Tile(2048, COLOR_2048, WHITE)
-        case _:
-            return Tile(0, COLOR_EMPTY, WHITE)
+    return tiles.get(value, Tile(0, COLOR_EMPTY, WHITE))
