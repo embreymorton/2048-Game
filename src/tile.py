4585e6e00704d041420c21c818b9f30eeccc856f
embreymorton <111366508+embreymorton@users.noreply.github.com>
2024-06-16 23:06:13 -0400

    Convert tile class to pydantic model

--- src/tile.py
+++ src/tile.py
@@ -1,7 +1,8 @@
+from pydantic import BaseModel
+from typing import Tuple
 from constants import *
 
-
-class Tile:
+class Tile(BaseModel):
     """
     Represents a tile on the game board.
 
@@ -11,31 +12,28 @@ class Tile:
         text_color (tuple): The text color of the tile.
         merged (bool): A flag indicating whether the tile has been merged with another tile.
     """
-
-    def __init__(self, value, bg_color, text_color, merged=False):
-        self.value = value
-        self.bg_color = bg_color
-        self.text_color = text_color
-        self.merged = merged
-
+    value: int
+    bg_color: Tuple[int, int, int]
+    text_color: Tuple[int, int, int]
+    merged: bool = False
 
 # Dictionary of Tile Objects
 tiles = {
-    0: Tile(0, COLOR_EMPTY, WHITE),
-    2: Tile(2, COLOR_2, BLACK),
-    4: Tile(4, COLOR_4, BLACK),
-    8: Tile(8, COLOR_8, WHITE),
-    16: Tile(16, COLOR_16, WHITE),
-    32: Tile(32, COLOR_32, WHITE),
-    64: Tile(64, COLOR_64, WHITE),
-    128: Tile(128, COLOR_128, WHITE),
-    256: Tile(256, COLOR_256, WHITE),
-    512: Tile(512, COLOR_512, WHITE),
-    1024: Tile(1024, COLOR_1024, WHITE),
-    2048: Tile(2048, COLOR_2048, WHITE)
+    0: Tile(value=0, bg_color=COLOR_EMPTY, text_color=WHITE),
+    2: Tile(value=2, bg_color=COLOR_2, text_color=BLACK),
+    4: Tile(value=4, bg_color=COLOR_4, text_color=BLACK),
+    8: Tile(value=8, bg_color=COLOR_8, text_color=WHITE),
+    16: Tile(value=16, bg_color=COLOR_16, text_color=WHITE),
+    32: Tile(value=32, bg_color=COLOR_32, text_color=WHITE),
+    64: Tile(value=64, bg_color=COLOR_64, text_color=WHITE),
+    128: Tile(value=128, bg_color=COLOR_128, text_color=WHITE),
+    256: Tile(value=256, bg_color=COLOR_256, text_color=WHITE),
+    512: Tile(value=512, bg_color=COLOR_512, text_color=WHITE),
+    1024: Tile(value=1024, bg_color=COLOR_1024, text_color=WHITE),
+    2048: Tile(value=2048, bg_color=COLOR_2048, text_color=WHITE)
 }
 
-def get_tile(value):
+def get_tile(value: int) -> Tile:
     """
     Create and return a Tile object based on the given value.
 
@@ -45,4 +43,4 @@ def get_tile(value):
     Returns:
         Tile: A Tile object with the specified value and appropriate colors.
     """
-    return tiles.get(value, Tile(0, COLOR_EMPTY, WHITE))
+    return tiles.get(value, Tile(value=0, bg_color=COLOR_EMPTY, text_color=WHITE))
